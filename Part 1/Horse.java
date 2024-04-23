bab03d2705a625a63e78d448b021dbe4ec6c4275
Ola <Hollabodey.kilaso@gmail.com>
2024-04-23 20:57:43 +0100

    Completed the horse.java methods

--- Part 1/Horse.java
+++ Part 1/Horse.java
@@ -9,7 +9,7 @@ public class Horse
 {
     //Fields of class Horse
     private String horse_name;
-    private char horseSymbol;
+    private char horse_symbol;
     private int distance_travelled;
     private boolean horse_status;
     private double horse_confidence;
@@ -20,7 +20,9 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+        this.horse_symbol = horseSymbol;
+        this.horse_name = horseName;
+        this.horse_confidence = horseConfidence;
     }
     
     
@@ -28,52 +30,66 @@ public class Horse
     //Other methods of class Horse
     public void fall()
     {
-        
+        String horse_status2 = "fallen";
+
+        if(horse_status2 == "fallen"){
+            this.horse_status = true;
+        } else {
+            this.horse_status = false;
+        }
     }
     
     public double getConfidence()
     {
-        
+        return horse_confidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return distance_travelled;
     }
     
     public String getName()
     {
-        
+        return horse_name;
     }
     
     public char getSymbol()
     {
-        
+        return horse_symbol;
     }
     
     public void goBackToStart()
     {
-        
+        this.distance_travelled = 0;
     }
     
     public boolean hasFallen()
     {
-        
+        if (this.horse_status){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void moveForward()
     {
-        
+        distance_travelled++;
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        if(newConfidence >= 0 && newConfidence <= 1){
+            this.horse_confidence = newConfidence;
+        } else{
+            System.out.println("Horse confidence must be either be 0 or 1");
+        }
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        this.horse_symbol = newSymbol;
     }
     
 }
