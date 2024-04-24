dfc274cdbeaa362c608848e2d129be58007fcb09
Ola <Hollabodey.kilaso@gmail.com>
2024-04-24 10:59:01 +0100

    Add UTest class with main method in Part 1/UTest.java

---
+++ Part 1/UTest.java
@@ -0,0 +1,9 @@
+public class UTest {
+    
+    public static void main(String[] args) {
+        Horse myHorse = new Horse('y', "Juniper", 0.2);
+        
+        System.out.println(myHorse.getSymbol());
+    }
+
+}
